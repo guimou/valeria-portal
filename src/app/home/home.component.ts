a117b589e36e2bf1e9eb9bca0895de2128bb6646
Guillaume Moutier <guillaume.moutier@gmail.com>
2019-03-17 14:07:27 -0400

    Home: Empty Card blanc, changement illustration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,14 +4,15 @@ import { EmptyStateConfig } from 'patternfly-ng';
 
 @Component({
     selector: 'app-home',
-    templateUrl: './home.component.html'
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
     emptyStateConfig: EmptyStateConfig = {
         iconStyleClass: 'fa fa-arrow-circle-left',
-        title: 'Valeria',
-        info: 'Click one of the links on the left to get started.'
+        title: 'Bienvenue dans Valeria',
+        info: 'Vous pouvez utiliser le menu à gauche pour accéder à vos applications ou à vos environnements.'
     };
 
     constructor(public route: ActivatedRoute) {
